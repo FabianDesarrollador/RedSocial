d5f696df8ff57d7d4c378282ed8b88e1ca330835
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-20 11:14:30 -0500

    Creacion del dominio, cadena coneccion y y el contexto de la base de datos

---
+++ RedSocial.Api/Models/PublicacionContext.cs
@@ -0,0 +1,19 @@
+﻿using RedSocial.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RedSocial.Api.Models
+{
+    public class PublicacionContext : DbContext
+    {
+        public PublicacionContext() : base("PublicacionConnection")
+        {
+
+        }
+
+        public DbSet<Publicacion> Publicaciones { get; set; }
+    }
+}
---
+++ RedSocial.Dominio/Publicacion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSocial.Dominio
+{
+    [Table("Publicacion")]
+    public class Publicacion
+    {
+        
+        
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            [MaxLength(50)]
+            public string Usuario { get; set; }
+            [Required]
+            [MaxLength(200)]
+            public string Descripcion { get; set; }
+            public DateTime FechaPublicacion { get; set; }
+            public int MeGusta { get; set; }
+            public int MeDisgusta { get; set; }
+            public int VecesCompartido { get; set; }
+            public bool EsPrivada { get; set; }
+        
+    }
+}
