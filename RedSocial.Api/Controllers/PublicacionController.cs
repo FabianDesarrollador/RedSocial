fcd9a8743aa161c4c85bdaed566a07e8c5c6f58e
FabianDesarrollador <desarrolladorsistemasfabian@gmail.com>
2018-10-20 11:44:17 -0500

    Instanciando BD

---
+++ RedSocial.Api/Controllers/PublicacionController.cs
@@ -0,0 +1,23 @@
+﻿using RedSocial.Api.Models;
+using RedSocial.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RedSocial.Api.Controllers
+{
+    public class PublicacionController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Publicacion> Get()
+        {
+            using (PublicacionContext context = new PublicacionContext())
+            {
+                return context.Publicaciones.ToList();
+            }
+        }
+    }
+}
